17c2d0c2f6fcc9dabea0129d7fbe4c71b238cc79
lajonss <gfs.b16@gmail.com>
2017-05-21 20:21:10 +0200

    model impl

--- endpoints/models.py
+++ endpoints/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class App(models.Model):
+    name = models.CharField(max_length=200)
+
+
+class UsedApp(models.Model):
+    RATINGS = (
+        ('1', '1'),
+        ('2', '2'),
+        ('3', '3'),
+        ('4', '4'),
+        ('5', '5')
+    )
+    app = models.ForeignKey(App)
+    user = models.ForeignKey(User)
+    time_summary = models.TimeField()
+    rating = models.CharField(max_length=1, choices=RATINGS)
+    notes = models.TextField()
+    events = None  # TODO
+
+
+class AppEvent(models.Model):
+    used_app = models.ForeignKey(UsedApp)
+    start_time = models.DateTimeField()
+    duration = models.TimeField()
+    finished = models.BooleanField()
