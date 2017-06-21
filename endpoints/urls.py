1ecdc9387e0b76ee49ea224927be9e70a5a86761
lajonss <gfs.b16@gmail.com>
2017-06-21 14:50:42 +0200

    urls update; regex is probably still wrong

--- endpoints/urls.py
+++ endpoints/urls.py
@@ -4,11 +4,11 @@ from endpoints import views
 urlpatterns = [
     url(r'^user/$', views.UserView.as_view()),
     url(r'^user/(?P<username>[\w.@+-]+)/$', views.UserView.as_view()),
-    url(r'^user/(?P<username>[\w.@+-]+)/(?P<appname>[\w.@+-]+)/$',
+    url(r'^user/(?P<username>[\w.@+-]+)/(?P<appname>[\w. @+-]+)/$',
         views.UsedAppView.as_view()),
-    url(r'^user/app/(?P<appname>[\w.@+-]+)/$', views.UsedAppView.as_view()),
-    url(r'^user/app/(?P<appname>[\w.@+-]+)/session/$',
+    url(r'^user/app/(?P<appname>[\w. @+-]+)/$', views.UsedAppView.as_view()),
+    url(r'^user/app/(?P<appname>[\w. @+-]+)/session/$',
         views.AppSessionView.as_view()),
     url(r'^app/$', views.AppView.as_view()),
-    url(r'^app/(?P<appname>[\w.@+-]+)/$', views.AppView.as_view()),
+    url(r'^app/(?P<appname>[\w. @+-]+)/$', views.AppView.as_view()),
 ]
