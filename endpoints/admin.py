1619bd76e094775452d71562d53f5a008648392b
lajonss <gfs.b16@gmail.com>
2017-06-21 14:34:27 +0200

    admin site configuration

--- endpoints/admin.py
+++ endpoints/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from endpoints.models import App, UsedApp, AppSession
+
+admin.site.register(App)
+admin.site.register(UsedApp)
+admin.site.register(AppSession)
