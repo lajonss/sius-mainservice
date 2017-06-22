38b8cae9b1289c41d8cad5b15ecb8ae0f5bb6338
lajonss <gfs.b16@gmail.com>
2017-06-22 20:17:55 +0200

    regex spaces inclusion

--- endpoints/urls.py
+++ endpoints/urls.py
@@ -4,12 +4,12 @@ from endpoints import views
 urlpatterns = [
     url(r'^user/$', views.UserView.as_view()),
     url(r'^user/(?P<username>[^\s]+)/$', views.UserView.as_view()),
-    url(r'^user/(?P<username>[^\s]+)/(?P<appname>[^\s]+)/$',
+    url(r'^user/(?P<username>[^\s]+)/(?P<appname>([^\s]| )+)/$',
         views.UsedAppView.as_view()),
-    url(r'^user/(?P<username>[^\s]+)/(?P<appname>[^\s]+)/$',
+    url(r'^user/(?P<username>[^\s]+)/(?P<appname>([^\s]| )+)/$',
         views.UsedAppView.as_view()),
-    url(r'^user/(?P<username>[^\s]+)/(?P<appname>[^\s]+)/session/$',
+    url(r'^user/(?P<username>[^\s]+)/(?P<appname>([^\s]| )+)/session/$',
         views.AppSessionView.as_view()),
     url(r'^app/$', views.AppView.as_view()),
-    url(r'^app/(?P<appname>[^\s]+)/$', views.AppView.as_view()),
+    url(r'^app/(?P<appname>([^\s]| )+)/$', views.AppView.as_view()),
 ]
